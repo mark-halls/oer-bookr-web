a002e530e122c3788be417dad489c9c09fa6ba05
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-21 13:57:47 -0600

    changed url to https

--- oer-front-end/src/Store/Actions/LoginActions.js
+++ oer-front-end/src/Store/Actions/LoginActions.js
@@ -4,17 +4,17 @@ export const LOGIN_USER_START = 'LOGIN_USER_START'
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
 export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
 
-const BASE_URL = "samirlilienfeld-oer-bookr.herokuapp.com/"
+const BASE_URL = "https://samirlilienfeld-oer-bookr.herokuapp.com/"
 
 export const loginUser = loginInput => dispatch => {
-	dispatch({type:LOGIN_USER_START});
+	dispatch({ type: LOGIN_USER_START });
 	axios
 		.post(`${BASE_URL}/login`, loginInput)
 		.then(res => {
-			dispatch({type: LOGIN_USER_SUCCESS})
+			dispatch({ type: LOGIN_USER_SUCCESS })
 			localStorage.setItem("token", res.data.access_token)
 		})
 		.catch(err => {
-			dispatch({type: LOGIN_USER_FAILURE, payload: err})
+			dispatch({ type: LOGIN_USER_FAILURE, payload: err })
 		})
 }
