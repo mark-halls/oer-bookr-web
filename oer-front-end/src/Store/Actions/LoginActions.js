d3901dfbd85bb1c485e9593908d2303d252d3dae
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-21 11:57:29 -0600

    loginUser action created

--- oer-front-end/src/Store/Actions/LoginActions.js
+++ oer-front-end/src/Store/Actions/LoginActions.js
@@ -1,7 +1,20 @@
+import axios from 'axios'
+
 export const LOGIN_USER_START = 'LOGIN_USER_START'
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
 export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
 
-export const loginUser = (param) => ({
+const BASE_URL = "samirlilienfeld-oer-bookr.herokuapp.com/"
 
-})
+export const loginUser = loginInput => dispatch => {
+	dispatch({type:LOGIN_USER_START});
+	axios
+		.post(`${BASE_URL}/login`, loginInput)
+		.then(res => {
+			dispatch({type: LOGIN_USER_SUCCESS})
+			localStorage.setItem("token", res.data.access_token)
+		})
+		.catch(err => {
+			dispatch({type: LOGIN_USER_FAILURE, payload: err})
+		})
+}
