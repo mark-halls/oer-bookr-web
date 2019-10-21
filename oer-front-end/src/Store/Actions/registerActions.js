44a909f2bc20bde46db6fcb112851e9d543ae3fd
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-21 11:37:43 -0600

    registerReducer created

---
+++ oer-front-end/src/Store/Actions/registerActions.js
@@ -0,0 +1,7 @@
+export const REGISTER_USER_START = 'REGISTER_USER_START'
+export const REGISTER_USER_SUCCESS = 'REGISTER_USER_SUCCESS'
+export const REGISTER_USER_FAILURE = 'REGISTER_USER_FAILURE'
+
+export const registerUser = (param) => ({
+
+})
