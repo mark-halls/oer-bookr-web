c024e92948083fc99d17646fb017fcb5362c948f
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-21 11:21:37 -0600

    loginReducer created

---
+++ oer-front-end/src/Store/Actions/LoginActions.js
@@ -0,0 +1,7 @@
+export const LOGIN_USER_START = 'LOGIN_USER_START'
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
+export const LOGIN_USER_FAULURE = 'LOGIN_USER_FAULURE'
+
+export const loginUser = (param) => ({
+
+})
