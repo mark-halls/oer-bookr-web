2247801095817274d2f0247ac4ba5839dd9f564e
Mark Halls <mark.halls@gmail.com>
2019-10-21 10:38:21 -0700

    Add nav component

--- oer-front-end/src/App.js
+++ oer-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import Nav from "./Components/Nav";
 import LoginPage from "./Views/LoginPage";
 import BookPage from "./Views/BookPage";
 import SignupPage from "./Views/SignupPage";
@@ -9,6 +10,7 @@ import HomePage from "./Views/HomePage";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Switch>
         <Route path="/login" render={props => <LoginPage {...props} />} />
         <Route path="/book/:id" render={props => <BookPage {...props} />} />
