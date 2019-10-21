c99127efd1b9ea03998176a7eefbdc233bf5a3a9
Mark Halls <mark.halls@gmail.com>
2019-10-21 15:07:28 -0700

    Fix links not redirecting due to bootstrap syntax

--- oer-front-end/src/Components/Nav/index.js
+++ oer-front-end/src/Components/Nav/index.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "reactstrap";
 
 const NavBar = () => {
   return (
     <Navbar color="light" light expand="md">
       <Nav className="ml-auto" navbar>
         <NavItem>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            Home
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/signup">Signup</NavLink>
+          <NavLink to="/signup" className="nav-link" activeClassName="active">
+            Signup
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/login" className="nav-link" activeClassName="active">
+            Login
+          </NavLink>
         </NavItem>
       </Nav>
     </Navbar>
