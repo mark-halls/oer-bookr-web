73af9222a42805365b397dff1f54016ef3822006
Mark Halls <mark.halls@gmail.com>
2019-10-22 15:01:38 -0700

    Load book by ID with reviews from api

--- src/Views/BookPage.js
+++ src/Views/BookPage.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import Book from "../Components/Book";
 import Review from "../Components/Review";
+import axios from "axios";
 
-const BookPage = () => {
-  const reviews = [
-    { review: "Much math, wow!", reviewer: "Doge", reviewid: 1 }
-  ];
-  const book = {
-    author: "test",
-    bookid: 1,
-    license: "MIT",
-    publisher: "testing clearing house",
-    title: "Calc 1",
-    reviews: reviews.length
-  };
+const BookPage = props => {
+  const [book, setBook] = useState();
+  const [reviews, setReviews] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://samirlilienfeld-oer-bookr.herokuapp.com/books/book/${id}`)
+      .then(res => {
+        setBook(res.data);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    if (book) {
+      async function fetchReviews() {
+        const reviewsArray = await Promise.all(
+          book.reviews.map(async review => {
+            const response = await axios.get(
+              `https://samirlilienfeld-oer-bookr.herokuapp.com/reviews/review/${review}`
+            );
+            return response.data;
+          })
+        );
+        setReviews(reviewsArray);
+      }
+
+      fetchReviews(book.reviews);
+    }
+  }, [book]);
 
   return (
     <div>
-      <Book {...book} />
-      {reviews.map(review => (
-        <Review {...review} key={review.reviewid} />
-      ))}
+      {book && <Book {...book} {...props} />}
+      <div>
+        <button>Write a review</button>
+      </div>
+      {reviews.length > 0 ? (
+        reviews.map(review => <Review {...review} key={review.reviewid} />)
+      ) : (
+        <p>No reviews</p>
+      )}
     </div>
   );
 };
