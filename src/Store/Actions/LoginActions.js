1b2351f9aa314e70aa14c95d0fc0b5293b252982
Mark Halls <mark.halls@gmail.com>
2019-10-22 11:46:37 -0700

    Fix api urls in actions and add email back into signup form

--- src/Store/Actions/LoginActions.js
+++ src/Store/Actions/LoginActions.js
@@ -1,21 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const LOGIN_USER_START = 'LOGIN_USER_START'
-export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
-export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
+export const LOGIN_USER_START = "LOGIN_USER_START";
+export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+export const LOGIN_USER_FAILURE = "LOGIN_USER_FAILURE";
 
-const BASE_URL = "https://samirlilienfeld-oer-bookr.herokuapp.com/"
+const BASE_URL = "https://samirlilienfeld-oer-bookr.herokuapp.com/";
 
 export const loginUser = loginInput => dispatch => {
-	dispatch({ type: LOGIN_USER_START });
-	axios
-		.post(`${BASE_URL}/login`, loginInput)
-		.then(res => {
-			dispatch({ type: LOGIN_USER_SUCCESS })
-			localStorage.setItem("token", res.data.access_token)
-		})
-		.catch(err => {
-			console.log(err)
-			dispatch({ type: LOGIN_USER_FAILURE, payload: err.response })
-		})
-}
+  dispatch({ type: LOGIN_USER_START });
+  axios
+    .post(`${BASE_URL}login`, loginInput)
+    .then(res => {
+      dispatch({ type: LOGIN_USER_SUCCESS });
+      localStorage.setItem("token", res.data.access_token);
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: LOGIN_USER_FAILURE, payload: err.response });
+    });
+};
