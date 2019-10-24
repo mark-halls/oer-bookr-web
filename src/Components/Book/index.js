a369b9d32646c5cd433c3786e36e58542578bb23
Mark Halls <mark.halls@gmail.com>
2019-10-24 11:13:55 -0700

    Add more isbn's and link to openlibrary.org

--- src/Components/Book/index.js
+++ src/Components/Book/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const bookToIsbn = {
+  Macroeconomics: "0538880457",
+  Precalculus: "0673158721",
+  "Electromagnetism, 2E": "0471927120",
+  Engineering: "0023071303",
+  "Linear algebra": "0201526751",
+  "The American Pageant": "0618247327",
+  Thermodynamics: "0070683050",
+  Microeconomics: "0471457698",
+  Calculus: "0534936245",
+  "Applied calculus": "0534419585",
   "Calculus: A Modeling Approach": "0201122162",
   "Chemistry: The Molecular Nature of Matter and Change": "0697396002",
   "Psychology: Themes and Variations": "0495093033",
@@ -18,10 +28,19 @@ const Book = props => {
         }-M.jpg`}
       />
       <Link to={`/book/${props.bookid}`}>Title: {props.title}</Link>
-      <p>Author: {props.author}</p>
+      <p>Author(s): {props.author}</p>
       <p>Publisher: {props.publisher}</p>
       <p>License: {props.license}</p>
       <p>Reviews: {props.reviews.length}</p>
+      <p>
+        <span>{`Read Now: `}</span>
+        <a
+          href={`https://openlibrary.org/isbn/${bookToIsbn[props.title]}`}
+          target="_blank"
+        >
+          openlibrary.org
+        </a>
+      </p>
     </div>
   );
 };
