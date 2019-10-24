9b63222f652efd2075b2a006aa7c4e0b224e2283
Mark Halls <mark.halls@gmail.com>
2019-10-24 12:21:32 -0700

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import "normalize.css";
 
 import Nav from "./Components/Nav";
 import LoginPage from "./Views/LoginPage";
@@ -8,11 +10,39 @@ import BookPage from "./Views/BookPage";
 import SignupPage from "./Views/SignupPage";
 import HomePage from "./Views/HomePage";
 
+const GlobalStyles = createGlobalStyle`
+html {
+  font-size: 62.5%;
+}
+
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Crimson+Text&display=swap');
+    font-family: 'Crimson Text', serif;
+    color: #222222;
+    background-color: #ffffef;
+    font-size: 2.25rem;
+  }
+
+  a {
+    color: #222222;
+  }
+
+  button {
+    background-color: #c8ab8c;
+    color: #222222;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`;
+
 function App() {
   const [loginToken, setLoginToken] = useState(localStorage.getItem("token"));
 
   return (
     <div className="App">
+      <GlobalStyles />
       <Nav token={loginToken} />
       <Switch>
         <Route
